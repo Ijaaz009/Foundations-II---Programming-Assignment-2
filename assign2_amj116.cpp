4c97f173666f27103655e97bbffa29be24e3c941
Alec Mason <Alecmace14@gmail.com>
2016-09-23 13:54:03 -0500

    Initial commit

---
+++ assign2_amj116.cpp
@@ -0,0 +1,50 @@
+/*
+File name: assign2_amj116.cpp
+Author: Alec (Mason) Jones
+Date: 2016-09-27
+Assignment Number: 1
+CS 2308.001 Fall 2016
+Instructor: Jill Seaman
+
+Description: "Electronic Store Inventory: Write a C++ program that
+will allow a user to manage the inventory of a small store that sells
+electronics like mobile phones, video games, and streaming devices." 
+(Jill Seaman, CS 2308 001 and 002 Fall 2016)
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+struct Product {
+    string productName;
+    int sku;
+    int quantity;
+    float price;
+};
+
+// 1. ADD a new product to the inventory (prompt user for input values)
+void addProduct() {}
+
+// 2. REMOVE a product from the inventory (by sku).
+void removeProduct() {}
+
+// 3. DISPLAY the inventory _by sku_
+void displayInventory() {}
+
+// 4. LOOKUP a product by sku
+void lookupInventory() {}
+
+// 5. QUIT
+
+int main () {
+
+    addProduct();
+    removeProduct();
+    displayInventory();
+    searchInventory();
+    
+   return 0;
+}
